ff77a0517ffaa8f02c7f3b3201e0037590dd8998
ircbloqcc <baskarhari4@gmail.com>
2021-07-01 20:54:58 +0530

    external package upload

---
+++ devices/device.js
@@ -0,0 +1,17 @@
+module.exports = [
+    // Buid-in device
+    'arduinoUno',
+    'arduinoNano',
+    'arduinoMini',
+    'arduinoLeonardo',
+    'arduinoMega2560',
+    'arduinoEsp32',
+    'arduinoEsp8266',
+    'microbit',
+    'microbitV2',
+    'makeyMakey',
+    // Third party
+    'ironKit_arduinoUno',
+    'QDPRobot_arduinoUnoUltra',
+    'QDPRobotC02_arduinoEsp32'
+];
